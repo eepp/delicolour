7cb11f3d3a765eca8b9c3ade2641f409db2d5cbe
Philippe Proulx <eeppeliteloop@gmail.com>
2014-02-15 13:29:58 -0500

    HexEntry: add clipboard support

--- delicolour/hex_entry.py
+++ delicolour/hex_entry.py
@@ -7,10 +7,12 @@ from gi.repository import Pango
 
 
 class HexEntry(Gtk.Entry):
-    def __init__(self, lower=True):
+    def __init__(self, lower=True, copy_hash=True):
         # parameters
         self._lower = lower
         self._user_on_change = None
+        self._copy_hash = copy_hash
+        self._clipboard_sel = Gdk.SELECTION_CLIPBOARD
 
         # build entry
         Gtk.Entry.__init__(self)
@@ -19,6 +21,9 @@ class HexEntry(Gtk.Entry):
         self.set_width_chars(6)
         self.connect('insert-text', self._on_insert_text)
         self._changed_handler = self.connect('changed', self._on_changed)
+        self.connect('cut-clipboard', self._on_cut)
+        self.connect('copy-clipboard', self._on_copy)
+        self.connect('paste-clipboard', self._on_paste)
 
     def _do_user_on_change(self):
         if self._user_on_change:
@@ -38,14 +43,52 @@ class HexEntry(Gtk.Entry):
         # notify user
         self._do_user_on_change()
 
+    def _set_clipboard(self, txt):
+        Gtk.Clipboard.get(self._clipboard_sel).set_text(txt, -1)
+
+    def _get_clipboard(self):
+        return Gtk.Clipboard.get(self._clipboard_sel).wait_for_text()
+
+    def _on_cut(self, entry):
+        self.stop_emission('cut-clipboard')
+
+    def _on_copy(self, entry):
+        text = self.get_text()
+        if len(text) in [3, 6]:
+            if self._copy_hash:
+                text = '#{}'.format(text)
+            self._set_clipboard(text)
+        self.stop_emission('copy-clipboard')
+
+    def _on_paste(self, entry):
+        text = self._get_clipboard()
+        if text is not None:
+            if text.startswith('#'):
+                text = text[1:]
+            if len(text) in [3, 6]:
+                self.set_text_no_emit(text)
+
+                # notify user
+                self._do_user_on_change()
+        self.stop_emission('paste-clipboard')
+
+    def set_lower(self, lower):
+        self._lower = lower
+
+    def set_copy_hash(self, copy_hash):
+        self._copy_hash = copy_hash
+
     def get_colour(self):
         return Colour.from_hex(self.get_text())
 
     def set_colour_no_emit(self, colour):
-        self.handler_block(self._changed_handler)
         text = colour.get_hex().upper()
         if self._lower:
             text = text.lower()
+        self.set_text_no_emit(text)
+
+    def set_text_no_emit(self, text):
+        self.handler_block(self._changed_handler)
         self.set_text(text)
         self.handler_unblock(self._changed_handler)
 
