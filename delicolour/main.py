4bc1a1e38c40b426f51451ca15b0ac52244d8d20
Philippe Proulx <eeppeliteloop@gmail.com>
2014-02-15 11:37:33 -0500

    Enable Ctrl+C

--- delicolour/main.py
+++ delicolour/main.py
@@ -1,8 +1,12 @@
 from delicolour import main_window
 from gi.repository import Gtk
-
+import signal
 
 def main():
+    # enable Ctrl+C
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    # main app
     win = main_window.MainWindow()
     win.connect('delete-event', Gtk.main_quit)
     win.show_all()
